cffcc1e8570fab01129d41c4954c891e32a319d8
lucaseo <lucaseo0147@gmail.com>
2021-04-06 23:58:30 +0900

    update tweet func.

--- tutorial-api/tutorial-flask/app.py
+++ tutorial-api/tutorial-flask/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 app.id_count = 1
 app.users = {}
 
-
+app.tweets=[]
 
 @app.route("/ping", methods=['GET']) # 엔드포인트를 @app.route 데코레이터를 통해 구현
 def ping():
@@ -20,3 +20,25 @@ def sign_up():
     app.id_count = app.id_count + 1
 
     return jsonify(new_user)
+
+
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet = payload['tweet']
+
+    if user_id not in app.users:
+        return "No user exist", 400
+
+    if len(tweet) > 300:
+        return "Tweet exceeds length of 300 chars", 400
+
+    user_id = int(payload['id'])
+    app.tweets.append({
+        'user_id': user_id,
+        'tweet': tweet
+    })
+
+    return '', 200
+
